205a7aae0cd62b1b263fa00a123c36bffbca3255
云云 <52269948+ngc660sec@users.noreply.github.com>
2022-08-21 21:33:50 +0800

    Update ip_collection.py

--- ip_collection.py
+++ ip_collection.py
@@ -2,6 +2,7 @@
 # 这里负责代理IP的采集工作
 from proxy_redis import ProxyRedis
 from multiprocessing import Process
+from concurrent.futures import ThreadPoolExecutor
 import requests
 from lxml import etree
 import time
@@ -12,20 +13,22 @@ headers = {
 
 
 def get_kuai_ip(red):
-    url = 'https://free.kuaidaili.com/free/intr/'
-    resp = requests.get(url=url, headers=headers)
-    tree = etree.HTML(resp.text)
-    trs = tree.xpath('//table//tr')
-    for tr in trs:
-        ip = tr.xpath('./td[1]/text()')  # IP
-        port = tr.xpath('./td[2]/text()')  # PORT
-        if not ip:
-            continue
-        ip = ip[0]
-        port = port[0]
-        proxy_ip = ip + ':' + port
-        # print(proxy_ip)
-        red.add_proxy_ip(proxy_ip)  # 增加新的ip地址
+    for i in range(1, 100):
+        url = f'https://free.kuaidaili.com/free/intr/{i}/'
+        resp = requests.get(url=url, headers=headers)
+        tree = etree.HTML(resp.text)
+        trs = tree.xpath('//table//tr')
+        for tr in trs:
+            ip = tr.xpath('./td[1]/text()')  # IP
+            port = tr.xpath('./td[2]/text()')  # PORT
+            if not ip:
+                continue
+            ip = ip[0]
+            port = port[0]
+            proxy_ip = ip + ':' + port
+            # print(proxy_ip)
+            red.add_proxy_ip(proxy_ip)  # 增加新的ip地址
+        time.sleep(20)
 
 
 def get_buzhidao_ip(red):
@@ -47,15 +50,15 @@ def get_buzhidao_ip(red):
 
 def run():  # 启动爬虫
     red = ProxyRedis()  # 创建好red存储
+    t = ThreadPoolExecutor(2)
     while True:
         try:
-            get_kuai_ip(red)  # 采集快代理
-            get_buzhidao_ip(red)  # 采集不知名的代理
+            t.submit(get_kuai_ip, red)  # 采集快代理
+            t.submit(get_buzhidao_ip, red)  # 采集不知名的代理
         except Exception as e:
             print('出现错误 >> ', e)
             continue
         time.sleep(60)  # 每分钟跑一次
-
 
 if __name__ == '__main__':
     run()
