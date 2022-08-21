546bc760c08e27c6d05f2a80ec5a37a10a61a9ad
云云 <52269948+ngc660sec@users.noreply.github.com>
2022-08-21 21:58:28 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,15 +1,30 @@
 # -*- coding:utf-8 -*-
 # 代理池配置文件
 
+# Redis地址
 REDIS_HOST = '127.0.0.1'
+
+# Rdeis端口
 REDIS_PORT = 6379
+
+# Redis数据库
 REDIS_DB = 5
+
+# Rdeis数据库密码
 REDIS_PASSWORD = "admin"
+
 # 存储在redis中的代理ip的key， 建议不更换
 REDIS_KEY = "proxy_ip"
+
+# api接口IP
+API_ADDRESS = '127.0.0.1'
+
+# api接口端口
+API_PORT = 10010
 
 # 默认IP分值
 DEFAULT_SCORE = 10
 
 # 满分
 MAX_SCORE = 100
+
