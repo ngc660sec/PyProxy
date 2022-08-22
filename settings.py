fc9e8f5b73394bff9975a7230eb9058bb202530f
云云 <52269948+ngc660sec@users.noreply.github.com>
2022-08-22 08:02:07 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ REDIS_HOST = '127.0.0.1'
 REDIS_PORT = 6379
 
 # Redis数据库
-REDIS_DB = 5
+REDIS_DB = 4
 
 # Rdeis数据库密码
 REDIS_PASSWORD = "admin"
@@ -22,9 +22,13 @@ API_ADDRESS = '127.0.0.1'
 # api接口端口
 API_PORT = 10010
 
+# 设置快代理的爬取页数
+KUAI_PAGE = 100
+
 # 默认IP分值
 DEFAULT_SCORE = 10
 
 # 满分
 MAX_SCORE = 100
+
 
